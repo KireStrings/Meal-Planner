88727c20952f97b272d2a95ba7ea951a082b45af
Erik Jaramillo <jaramilloerik21@gmail.com>
2025-07-07 16:30:59 +0200

    feat: show the meal plan according to the current user.

--- app/routes/meal_plan.py
+++ app/routes/meal_plan.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from ..models import MealPlan
 from flask_login import current_user, login_required
 
 meal_plan = Blueprint('meal_plan', __name__)
@@ -6,4 +7,5 @@ meal_plan = Blueprint('meal_plan', __name__)
 @meal_plan.route('/my-plan', methods=['GET'])
 @login_required
 def my_plan():
-    return render_template('meal-plan.html', user=current_user)
+    user_plans = current_user.meal_plans.order_by(MealPlan._input_date.desc()).all()
+    return render_template('meal-plan.html', meal_plans=user_plans)

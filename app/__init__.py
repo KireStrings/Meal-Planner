ec171b7c73510f2715a73ee45cdd059c5b367fdb
Erik Jaramillo <jaramilloerik21@gmail.com>
2025-07-07 16:25:59 +0200

    build: load dotenv file for api key instantiation

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from jinja2 import pass_context
+from dotenv import load_dotenv
+load_dotenv(override=True)
 
 db = SQLAlchemy()
 migrate = Migrate()

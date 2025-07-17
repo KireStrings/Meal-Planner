04f9d1a3a19a632ea326b62d3fdcb39e34edd575
frankba <62514408+frankbits@users.noreply.github.com>
2025-07-17 21:16:41 +0200

    refactor: removing unused MealPlanForm

--- app/forms.py
+++ app/forms.py
@@ -18,10 +18,3 @@ class AccountForm(FlaskForm):
     email = EmailField('Email', validators=[InputRequired(), DataRequired(), Email()])
     password = PasswordField('New Password', validators=[])
     submit = SubmitField('Save Changes')
-
-class MealPlanForm(FlaskForm):
-    calories_per_day = IntegerField('Calories per day', validators=[DataRequired(), NumberRange(min=500, max=10000)])
-    num_meals = IntegerField('Number of meals', validators=[DataRequired(), NumberRange(min=2, max=6)])
-    avg_price = DecimalField('Average price per meal ($)', validators=[DataRequired()])
-    diet_type = SelectField('Diet type', choices=[('any', 'Any'), ('vegetarian', 'Vegetarian'), ('vegan', 'Vegan'), ('keto', 'Keto')])
-    submit = SubmitField('Generate Plan')
